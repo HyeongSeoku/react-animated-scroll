a1f81a2baafe1c860b7449007d95ba21c910d7a8
김형석 <gudtjr3437@jobkorea.co.kr>
2023-07-16 16:16:24 +0900

    Add :white_check_mark: direction & initialize destination function

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,43 @@
 import { createElement, ReactNode } from "react";
 
 type Target = string | number | HTMLElement;
+type Direction = "vertical" | "horizontal";
 
 interface AnimatedScrollProps {
     target: Target;
     children?: ReactNode;
     duration?: number;
+    direction?: Direction;
     beforeScroll?: () => void;
     afterScroll?: () => void;
 }
+
+const initializeTo = (target: Target, direction: Direction) => {
+    switch (typeof target) {
+        case "number":
+            return target;
+        case "string":
+            return direction === "vertical"
+                ? document.getElementById(target)?.getBoundingClientRect()?.y ||
+                      0
+                : document.getElementById(target)?.getBoundingClientRect()?.x ||
+                      0;
+        default:
+            return direction === "vertical"
+                ? target.getBoundingClientRect()?.y || 0
+                : target.getBoundingClientRect()?.x || 0;
+    }
+};
 
 const AnimatedScroll = ({
     target,
     children,
     duration = 0,
+    direction = "vertical",
     beforeScroll,
     afterScroll,
 }: AnimatedScrollProps) => {
-    const to =
-        typeof target === "number"
-            ? target
-            : typeof target === "string"
-            ? document.getElementById(target)?.getBoundingClientRect()?.y || 0
-            : target.getBoundingClientRect()?.y || 0;
+    const to = initializeTo(target, direction);
     const from = window.scrollY;
 
     const scroll = () => {
